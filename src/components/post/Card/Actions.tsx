ecab59e6a978bfe09a42e84d9cc4d0c6bf41249e
m0bar3z <amobarez00@gmail.com>
2024-08-30 16:42:44 +0330

    feat: like and dislike for posts

---
+++ src/components/post/Card/Actions.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { type FC, memo, useState } from "react";
+import { HeartIcon, ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import { HeartIcon as SolidHeartIcon } from "@heroicons/react/24/solid";
+import { useAppDispatch } from "@/lib/hooks";
+import { addLike, deleteLike } from "@/lib/features/posts/postsSlice";
+
+const Actions: FC<{ id: string }> = ({ id }) => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const dispatch = useAppDispatch();
+
+  const handleAddLike = () => {
+    dispatch(addLike(id));
+    setIsLiked(true);
+  };
+
+  const handleRemoveLike = () => {
+    dispatch(deleteLike(id));
+    setIsLiked(false);
+  };
+
+  return (
+    <div className="flex gap-2">
+      {isLiked ? (
+        <SolidHeartIcon
+          className="icon-btn text-red-500 hover:text-red-500"
+          onClick={handleRemoveLike}
+        />
+      ) : (
+        <HeartIcon
+          className="icon-btn transition-all duration-300 active:scale-125"
+          onClick={handleAddLike}
+        />
+      )}
+
+      <ChatBubbleLeftIcon className="icon-btn" onClick={() => console.log("new comment")} />
+    </div>
+  );
+};
+
+export default memo(Actions);
--- src/components/post/Card/index.tsx
+++ src/components/post/Card/index.tsx
@@ -1,13 +1,11 @@
-"use client";
-
-import Image from "next/image";
 import { type FC } from "react";
-import { HeartIcon, ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 import type { PostStateType } from "@/lib/features/posts/postsSlice";
+import Actions from "./Actions";
 
 type CardPropsType = PostStateType;
 
-const Card: FC<CardPropsType> = ({ caption, code, display_src, id, likes }) => {
+const Card: FC<CardPropsType> = ({ caption, display_src, id, likes }) => {
   return (
     <div className="card card-compact relative m-auto w-2/6 rounded-lg border-y-[1px] border-slate-400 bg-base-100 shadow-xl">
       <figure>
@@ -20,10 +18,7 @@ const Card: FC<CardPropsType> = ({ caption, code, display_src, id, likes }) => {
         />
       </figure>
       <div className="card-body">
-        <div className="flex gap-2">
-          <HeartIcon className="icon-btn" onClick={() => console.log("new like")} />
-          <ChatBubbleLeftIcon className="icon-btn" onClick={() => console.log("new comment")} />
-        </div>
+        <Actions id={id} />
         <div className="font-bold">{likes} likes</div>
 
         <div className="">
